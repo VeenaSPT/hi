3d2e25cd97755bd57476195888dacdf30fad8645
Veena N <114400616+VeenaSPT@users.noreply.github.com>
2025-03-19 12:24:22 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ class first
   {
     public static void main(String[] args)
     {
-      System.out.println("hiiiiiiiiiiiiiii");
+      System.out.println("hiiiiiiiiiiiiiii welcome to jenkins");
     }
   }

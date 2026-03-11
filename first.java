0640c81cc7b4ea1af857121484aad793ca8b5fac
Veena N <114400616+VeenaSPT@users.noreply.github.com>
2026-03-11 09:39:06 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ class first
   {
     public static void main(String[] args)
     {
-      System.out.prin("hello welcome to jenkins!!!!");
+      System.out.println("hello welcome to jenkins!!!!");
     }
-  
+  }

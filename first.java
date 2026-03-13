509afa786bd4c370c7d762bfe2ffdde7ddb7f3b9
Veena N <114400616+VeenaSPT@users.noreply.github.com>
2026-03-13 11:51:39 +0530

    Fix typo in print statement in first.java

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ class first
   {
     public static void main(String[] args)
     {
-      System.out.pri("hello welcome to jenkins!!!!");
+      System.out.println("hello welcome to jenkins!!!!");
       System.out.println("welcom to SE lab");
     }
-  
+  }
